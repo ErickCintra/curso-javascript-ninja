7a806eaeb7711b341102d1cc4643cf0e1e941a3c
ErickCintra <erickcintra.dev@gmail.com>
2019-12-19 23:54:27 -0300

    exercicios lista 1 concluidos

---
+++ #1/challenge.js
@@ -0,0 +1,31 @@
+//#1
+var myvar;
+//#2
+var myvar = 10;
+//#3
+var soma;
+soma = 15 + 8;
+//#4
+soma += 1;
+//#5
+soma *= 3;
+//#6
+24
+//#7
+var souninja = true;
+//#8
+var comida;
+comida = ['arroz','feijão','ovo'];
+//#9
+comida[1];
+//#10
+soma === myvar;
+//#11
+myvar <= soma;
+//#12
+function divisao(x, y) {
+	return x + y;
+}
+divisao();
+//#13
+divisao(10,2);
